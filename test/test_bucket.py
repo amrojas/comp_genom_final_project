9ae62e5ec9e697d6739c85f9a2b26a54c70cb92b
Omar Ahmed <32006908+oma219@users.noreply.github.com>
2020-10-22 17:07:05 -0400

    added some simple tests for standard bucket

---
+++ test/test_bucket.py
@@ -0,0 +1,56 @@
+"""
+Description: Contains the unit tests for Bucket class
+
+"""
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+import bucket_classes
+
+
+def test_bucket_creation():
+    """ Makes sure the bucket instance variables are as expected when created """
+    num_entries = 4
+    test_bucket = bucket_classes.Bucket(num_entries)
+    assert test_bucket.size == num_entries
+    assert test_bucket.bucket == []
+
+def test_insert():
+    """ Makes sure we insert correctly and we cannot insert at full bucket """
+    num_entries = 2
+    test_bucket = bucket_classes.Bucket(num_entries)
+    test_bucket.insert(0b1111)
+    assert test_bucket.contains(0b1111) == True
+    assert test_bucket.insert(0b1111) == True
+    assert test_bucket.insert(0b1011) == False
+
+def test_bucket_full():
+    """ Ensures that a bucket's isFull() works as expected """
+    num_entries = 2
+    test_bucket = bucket_classes.Bucket(num_entries)
+    test_bucket.insert(0b0111) 
+    test_bucket.insert(0b0001)
+    assert test_bucket.isFull() == True
+    
+    test_bucket.remove(0b0001)
+    assert test_bucket.isFull() == False
+
+def test_contains():
+    """ Ensures that contains works after insertions and removals, and uninserted items """
+    num_entries = 2
+    test_bucket = bucket_classes.Bucket(num_entries)
+    test_bucket.insert(0b1111)
+    assert test_bucket.contains(0b1111) == True
+    assert test_bucket.contains(0b1110) == False
+    test_bucket.remove(0b1111)
+    assert test_bucket.contains(0b1111) == False
+
+def test_remove():
+    """ """
+    num_entries = 2
+    test_bucket = bucket_classes.Bucket(num_entries)    
+    test_bucket.insert(0b0011)
+    assert test_bucket.remove(0b0011) == True
+    assert test_bucket.remove(0b0011) == False
+    
