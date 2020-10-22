2529a2ed2c05a41554f3f790fcd1f071fa6596c9
Omar Ahmed <32006908+oma219@users.noreply.github.com>
2020-10-22 17:08:09 -0400

    added basic methods for standard bucket

--- src/bucket_classes.py
+++ src/bucket_classes.py
@@ -1,16 +1,39 @@
 """
 Description: Contains the basic implementation for a Bucket in Cuckoo Filter
-
 """
 
 class Bucket:
 
     def __init__(self, entries):
+        """
+        Creates a standard bucket.
+
+            size --> how many fingerpritns will this bucket store
+            bucket --> python list that stores fingerprints
+        """
         self.size = entries
         self.bucket = []
     
     def isFull(self):
         if len(self.bucket) >= self.size:
+            return True
+        return False
+    
+    def contains(self, fp):
+        return (fp in self.bucket)
+    
+    def insert(self, fp):
+        """ Important point is that duplicate items are allowed as
+            described in Fan et. al (2014)
+        """ 
+        if not self.isFull():
+            self.bucket.append(fp)
+            return True
+        return False
+    
+    def remove(self, fp):
+        if fp in self.bucket:
+            self.bucket.remove(fp)
             return True
         return False
 
