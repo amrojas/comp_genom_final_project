83e2f07e3583a57e405dcbfaee8dd099a247580a
Omar Ahmed <32006908+oma219@users.noreply.github.com>
2020-10-23 02:00:03 -0400

    added swap_with_random_entry method for insert in cuckoo filter

--- src/bucket_classes.py
+++ src/bucket_classes.py
@@ -1,6 +1,7 @@
 """
 Description: Contains the basic implementation for a Bucket in Cuckoo Filter
 """
+import random
 
 class Bucket:
 
@@ -23,7 +24,8 @@ class Bucket:
         return (fp in self.bucket)
     
     def insert(self, fp):
-        """ Important point is that duplicate items are allowed as
+        """ 
+            Important point is that duplicate items are allowed as
             described in Fan et. al (2014)
         """ 
         if not self.isFull():
@@ -36,5 +38,12 @@ class Bucket:
             self.bucket.remove(fp)
             return True
         return False
+    
+    def swap_with_random_entry(self, fp_to_insert):
+        chosen_fp = random.choice(self.bucket)
+        self.remove(chosen_fp)
+        self.insert(fp_to_insert)
+        return chosen_fp
+
 
     
