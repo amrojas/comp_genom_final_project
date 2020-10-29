bec934db1bf9a0c00deb80a020cb368b4a316b16
Omar Ahmed <32006908+oma219@users.noreply.github.com>
2020-10-28 20:43:31 -0400

    added attributes to be able to calculate load factor

--- src/cuckoo_filter.py
+++ src/cuckoo_filter.py
@@ -22,42 +22,43 @@ class CuckooFilter:
         self.bucket_size = bucket_size
         self.max_iter = max_iter
         self.filter = [bucket_classes.Bucket(self.bucket_size) for i in range(num_buckets)]
-
+        self.num_items_in_filter = 0
+        self.total_capacity = self.bucket_size * self.num_buckets
 
     @staticmethod
     def get_hash_value(item):
         hash_sha256 = hashlib.sha256(str(item).encode('utf-8'))
         hash_result = hash_sha256.digest()
-        hash_result_int = int.from_bytes(hash_result, byteorder='big')  
+        hash_result_int = int.from_bytes(hash_result, byteorder='big')
         return hash_result_int
-    
+
     @staticmethod
     def get_fingerprint(item, fp_size):
-        """ 
+        """
             Gets a fingerprint for item trying to be inserted by hashing
             and taking the fp.size least significant bits. Returns an integer.
         """
 
         #Create a bit mask of self.fp_size bits
         mask_bits_int = int(("1" * fp_size), 2)
-        mask_bytes = mask_bits_int.to_bytes((mask_bits_int.bit_length() // 8) + 1, byteorder='big')   
+        mask_bytes = mask_bits_int.to_bytes((mask_bits_int.bit_length() // 8) + 1, byteorder='big')
 
         #Get hash value for the item trying to be inserted
         hash_result_int = CuckooFilter.get_hash_value(item)
- 
+
         #Get fingerprint with bitwise-AND
         fp = hash_result_int & mask_bits_int
         return fp
-    
+
     def get_fp_and_index_positions(self, item):
         fingerprint = CuckooFilter.get_fingerprint(item, self.fp_size)
-        
+
         hash_x = CuckooFilter.get_hash_value(item)
         hash_fp = CuckooFilter.get_hash_value(fingerprint)
 
         index_one = hash_x % self.num_buckets
         index_two = (index_one ^ hash_fp) % self.num_buckets
-        
+
         return [fingerprint, index_one, index_two]
 
     def insert(self, item):
@@ -71,9 +72,11 @@ class CuckooFilter:
         #Try to insert into one of those two buckets
         if not self.filter[index_one].isFull():
             self.filter[index_one].insert(fingerprint)
+            self.num_items_in_filter += 1
             return True
         elif not self.filter[index_two].isFull():
             self.filter[index_two].insert(fingerprint)
+            self.num_items_in_filter += 1
             return True
 
         #Try to relocate some of the items in bucket
@@ -86,28 +89,26 @@ class CuckooFilter:
 
             if not self.filter[index].isFull():
                 self.filter[index].insert(fingerprint)
+                self.num_items_in_filter += 1
                 return True
-        
+
         #We have failed to insert, filter is full
         return False
-    
+
     def contains(self, item):
         fingerprint, index_one, index_two = self.get_fp_and_index_positions(item)
 
         if self.filter[index_one].contains(fingerprint) or self.filter[index_two].contains(fingerprint):
-            return True     
+            return True
         return False
-    
+
     def delete(self, item):
         fingerprint, index_one, index_two = self.get_fp_and_index_positions(item)
 
         if self.filter[index_one].contains(fingerprint):
-            return self.filter[index_one].remove(fingerprint)    
+            self.num_items_in_filter -= 1
+            return self.filter[index_one].remove(fingerprint)
         elif self.filter[index_two].contains(fingerprint):
-            return self.filter[index_two].remove(fingerprint)        
+            self.num_items_in_filter -= 1
+            return self.filter[index_two].remove(fingerprint)
         return False
-
-
-
-
-
