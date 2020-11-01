2ff41a49f1c2a0814db2990669ea9c4eda3de350
Omar Ahmed <32006908+oma219@users.noreply.github.com>
2020-11-01 11:11:21 -0500

    added constructor test for cuckoo filter with stash

--- test/test_cuckoo_filter.py
+++ test/test_cuckoo_filter.py
@@ -10,26 +10,37 @@ import cuckoo_filter
 
 def test_construction():
     """ Ensures the cuckoo filter is constructed as we would expect """
-    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8)
+    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8, 1, 500)
     assert cuckooFilter.num_buckets == 10
     assert cuckooFilter.fp_size == 8
 
 def test_insert():
     """ Ensures the cuckoo filter can insert items we should be able to """
-    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8, 1)
+    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8, 1, 500)
     assert cuckooFilter.insert("GCGTTT") == True
     assert cuckooFilter.insert("GCGTTT") == True
 
 def test_contains():
     """ Ensures you can find inserted strings and not find strings not inserted"""
-    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8)
+    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8, 1, 500)
     cuckooFilter.insert("GCGTTT")
     assert cuckooFilter.contains("GCGTTT") == True
     assert cuckooFilter.contains("TTT") == False
 
 def test_delete():
     """ Ensures you can delete strings present in filter and cannot delete those not in filter """
-    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8)
+    cuckooFilter = cuckoo_filter.CuckooFilter(10, 8, 1, 500)
     cuckooFilter.insert("GCGTTT")
     assert cuckooFilter.delete("GCGTTT") == True
     assert cuckooFilter.delete("GCGTTT") == False
+
+def test_construction_stash():
+    """ Ensures the cuckoo filter with stash is constructed as we would expect """
+    cuckooFilterStash = cuckoo_filter.CuckooFilterStash(10, 8, 1, 500, 64)
+    assert cuckooFilterStash.num_buckets == 10
+    assert cuckooFilterStash.fp_size == 8
+    assert cuckooFilterStash.total_capacity == 74
+    assert cuckooFilterStash.stash_size == 64
+
+
+
